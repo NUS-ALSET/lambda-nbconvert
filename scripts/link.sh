e90c0fd13e0a673807ddff4e602c5e37168a3b31
Leon Manukyan <leon.manukyan@gmail.com>
2019-02-19 16:37:37 +0400

    scripts/link.sh can be run in TTY-less environment

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,6 +3,6 @@
 rm -rf build overlay
 mkdir -p build/code
 mkdir overlay
-docker run --user=$UID --entrypoint=/bin/bash -it --rm -v $PWD:/var/task lambci/lambda:build-python3.6 ./scripts/package.sh
+docker run --user=$UID --entrypoint=/bin/bash --rm -v $PWD:/var/task lambci/lambda:build-python3.6 ./scripts/package.sh
 mv build/code/tensorflow overlay
 #ln -sv main.py build/code/
