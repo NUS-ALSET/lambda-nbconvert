7e772ac8236abf0d962a19b718f4e4463819c790
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-06-27 14:31:30 +0800

    Adding results as a json in post response

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from timeit import default_timer as timer
+import base64
+import json
+from io import StringIO
+from nbconvert.preprocessors import ExecutePreprocessor
+import nbformat
+import logging
 import os
 import sys
 import ast
@@ -18,35 +25,27 @@ sys.path.append(CURRENT_DIR)
 sys.path.append(BUILD_DIR)
 sys.path.append(OVERLAY_DIR)
 
-os.environ['PYTHONPATH'] = os.environ['PYTHONPATH'] + ":" + CURRENT_DIR + ":" + BUILD_DIR + ":" + OVERLAY_DIR
+os.environ['PYTHONPATH'] = os.environ['PYTHONPATH'] + \
+    ":" + CURRENT_DIR + ":" + BUILD_DIR + ":" + OVERLAY_DIR
 
 
-
-
-import logging
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-from io import StringIO
-import json
-import base64
-
-from timeit import default_timer as timer
 logger = logging.getLogger(__name__)
 
 
 def return_result_json():
     os.chdir('/tmp')
     if(os.path.isfile('results.json')):
-        result_file_reader = open('results.json','r')
+        result_file_reader = open('results.json', 'r')
         result_file = result_file_reader.read()
         return result_file
+
 
 def base64_decode_and_persist(filename, contents):
     os.chdir('/tmp')
     decoded = base64.b64decode(contents)
     with open(filename, 'wb') as outfile:
         outfile.write(decoded)
-        
+
 
 def save_files_to_temp_dir(files):
     for key in files:
@@ -66,7 +65,8 @@ def execute_notebook(source):
     nb = nbformat.read(in_memory_source, as_version=4)
 
     logger.debug("Launching kernels")
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python3', allow_errors=True)
+    ep = ExecutePreprocessor(
+        timeout=600, kernel_name='python3', allow_errors=True)
     ep.preprocess(nb, {'metadata': {'path': '/tmp/'}})
 
     ex = StringIO()
@@ -75,14 +75,19 @@ def execute_notebook(source):
     logger.debug("Returning results")
     return ex.getvalue()
 
+
 homepage = ""
 with open('index.html') as f:
-    homepage=f.read()
+    homepage = f.read()
+
 
 def populate_cors(response):
-    response['headers']['Access-Control-Allow-Origin'] = os.environ.get('CORS_DOMAIN', '*')
-    response['headers']['Access-Control-Allow-Headers'] = os.environ.get('CORS_HEADERS', 'Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token')
-    response['headers']['Access-Control-Allow-Methods'] = os.environ.get('CORS_METHODS', 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT')
+    response['headers']['Access-Control-Allow-Origin'] = os.environ.get(
+        'CORS_DOMAIN', '*')
+    response['headers']['Access-Control-Allow-Headers'] = os.environ.get(
+        'CORS_HEADERS', 'Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token')
+    response['headers']['Access-Control-Allow-Methods'] = os.environ.get(
+        'CORS_METHODS', 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT')
 
 
 def handler(event, context):
@@ -106,18 +111,17 @@ def handler(event, context):
                 'Content-Type': 'application/json',
             }
         }
-        populate_cors(response)     
-        return response   
+        populate_cors(response)
+        return response
     else:
         request_body = json.loads(event["body"])
 
         notebook_source = request_body['notebook']
-        attached_files = request_body['files'] if 'files' in request_body else {}
+        attached_files = request_body['files'] if 'files' in request_body else {
+        }
         save_files_to_temp_dir(attached_files)
 
-
-
-        start = timer()        
+        start = timer()
         result = execute_notebook(json.dumps(notebook_source))
         result = json.loads(result)
 
@@ -127,7 +131,7 @@ def handler(event, context):
         duration = end - start
         response = {
             "statusCode": 200,
-            "body": json.dumps({"duration": duration,"ipynb": result, "result": exec_result}),
+            "body": json.dumps({"duration": duration, "ipynb": result, "result": exec_result, "results": json.loads(exec_result)}),
             "headers": {
                 'Content-Type': 'application/json',
             }
